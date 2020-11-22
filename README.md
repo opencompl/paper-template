4afa0cfe70a366f77cc10096af8515e894e70afa
Tobias Grosser <tobias@grosser.es>
2020-11-22 09:06:11 +0000

    Build video from pdfs when new release is tagged

--- README.md
+++ README.md
@@ -25,6 +25,9 @@ the following useful features:
       of room for comments
     - Pieces of text can be addressed specifically by underlining this text
     - Comments in float enviroments (e.g., figure)
+  - Paper Video
+    Whenever a commit is tagged, the automatically built PDFs are combined into a release
+    video that illustrates the writing process.
   - GitHub Pages Support
     - The rebuilt repository will be pushed to GitHub Pages to such that the PDFs are
       also available for users without a GitHub Account. The PDFs are availble as
---
+++ tools/create-video.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+
+from subprocess import run
+import os
+import math
+import sys
+import shutil
+
+def getPageNumber(name):
+    files = os.listdir(os.path.dirname(name))
+    files = filter(lambda f: f.startswith(os.path.basename(name)), files)
+    files = filter(lambda f: f.endswith('.png'), files)
+    files = list(files)
+    return len(files)
+
+def removePNGFiles(directory):
+    files = os.listdir(directory)
+    files = filter(lambda x: x.find(".png") != -1, files)
+    for f in files:
+      os.remove(directory + '/' + f)
+
+def createImageOfPaper(path, width = 6, pages = 18):
+    print(path)
+    removePNGFiles(os.path.dirname(path))
+
+    name = path
+
+    cmd = ['convert', '-density', '300', path, '-resize', '40%', '-background', 'white', '-alpha', 'remove', '-alpha', 'off', name + ".png"]
+    run(cmd)
+
+    pageNumber = getPageNumber(path);
+    if pageNumber == 1:
+        shutil.copyfile(name + ".png", name + "-0.png")
+
+    for i in range(pageNumber, pages):
+        cmd = ['convert', '-size', '1020x1320', 'xc:white', name + '-' + str(i) + '.png']
+        run(cmd)
+
+    for row in range(0, math.ceil(pages / width)):
+        cmd = ['convert']
+        for imageId in range(row * width, min((row + 1) * width, pages)):
+            cmd.append(name + "-" + str(imageId) + ".png")
+        cmd.append("+append")
+        cmd.append(name + "-row-" + str(row) + ".png")
+        run(cmd)
+
+    cmd = ['convert']
+    for row in range(0, math.ceil(pages / width)):
+      cmd.append(name + "-row-" + str(row) + ".png")
+    cmd.append("-append")
+    cmd.append(name + "-full.png")
+    run(cmd)
+
+def getReleases(path):
+    releases = os.listdir(path)
+    releases = filter(lambda x: x.find('master-release') != -1, releases)
+    releases = map(lambda x: x.replace('master-release-', ''), releases)
+    releases = map(lambda x: int(x), releases)
+    releases = list(releases)
+    releases.sort()
+    releases = map(lambda x: path + 'master-release-' + str(x) + "/blind.pdf", releases)
+    releases = list(releases)
+    releases = filter(os.path.exists, releases)
+    releases = list(releases)
+    return list(releases)
+
+
+def createImages(path):
+    releases = getReleases(path)
+
+    for release in releases:
+        createImageOfPaper(release)
+
+def moveImages(path):
+    removePNGFiles(path)
+    releases = getReleases(path)
+    releases = map(lambda x: x + "-full.png", releases)
+    releases = filter(os.path.exists, releases)
+
+    idx = 0
+    for release in releases:
+        shutil.copyfile(release, path + ("still-%05d.png" % idx))
+        idx = idx + 1
+
+def createVideo(path):
+    run(['rm', path + 'video.mp4'])
+    cmd = ['ffmpeg', '-r', '1', '-i', path + 'still-%05d.png', '-vcodec', 'libx264', '-crf', '25', '-pix_fmt', 'yuv420p', path + 'video.mp4']
+    run(cmd)
+    print(path + 'video.mp4')
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Create a Video from a GitHub Release Download of tobiasgrosser/paper-template.')
+parser.add_argument('repo', help='owner/repo, e.g., tobias-grosser/paper-template')
+
+args = parser.parse_args()
+imagePath = args.repo + "/refs/heads/"
+createImages(imagePath)
+moveImages(imagePath)
+createVideo(imagePath)
