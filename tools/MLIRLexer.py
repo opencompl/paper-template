b307215e26aacb1e8d7093c991f051ba06cd1f29
Michel Weber <michel.weber@inf.ethz.ch>
2022-09-06 14:49:40 +0200

    add more functionality to lexer

--- tools/MLIRLexer.py
+++ tools/MLIRLexer.py
@@ -1,6 +1,7 @@
 from pygments.lexer import RegexLexer, bygroups
 from pygments.token import *
 
+
 class MLIRLexer(RegexLexer):
     name = 'MLIR'
     aliases = ['mlir']
@@ -9,16 +10,29 @@ class MLIRLexer(RegexLexer):
     tokens = {
         'root': [
             (r'//.*?\n', Comment),
-            (r'%[^ ]*', Name.Variable),
+            (r'%[^[ )]*]*', Name.Variable),
             (r'@[^(]*', Name.Function),
-            (r'\^[^(]*', Name.Label),
-            (r'(=)( +)([a-z]+)(\.)([a-z]+)', bygroups(Operator, Text, Name.Namespace, Text, Keyword.Function)),
-            (r'(!)([a-z]+)(\.)([a-z]+)', bygroups(Operator, Name.Namespace, Text, Keyword.Type)),
+            (r'\^[^(:\]]*', Name.Label),
+            (r'(=)( +)([a-z_]+)(\.)([a-z_]+)',
+             bygroups(Operator, Text, Name.Namespace, Text, Keyword.Function)),
+            (r'([a-z_]+)(\.)([a-z_]+)',
+             bygroups(Name.Namespace, Text, Keyword.Function)),
+            (r'(!)([a-z_]+)(\.)([a-z0-9_]+)',
+             bygroups(Operator, Name.Namespace, Text, Keyword.Type)),
+            (r'(!)([a-z0-9]+)', bygroups(Operator, Keyword.Type)),
             (r'(\n|\s)+', Text),
+            (r'([{\[])([a-z_]+)( = +)([a-z0-9">=]+)',
+             bygroups(Text, Name.Attribute, Text, Name.Tag)),
+            (r'(\[)([a-z]+)', bygroups(Text, Name.Variable)),
+            (r'(, +)([a-z_]+)( = +)([a-z0-9">=]+)',
+             bygroups(Text, Name.Attribute, Text, Name.Tag)),
+            (r'(, +)([a-z_]+)( = +)', bygroups(Text, Name.Attribute, Text)),
             (r'[=<>{}:\[\]()*.,!]|x\b', Punctuation),
+            (r'(...)', Text),
             (r'def', Keyword),
         ]
     }
+
 
 # This Lexer is a hack that enables a custom style where only operation names
 # are highlighted and comments as well as dialect names are printed in light
@@ -34,8 +48,10 @@ class MLIRLexerOnlyOps(RegexLexer):
             (r'%[^ ]*', Text),
             (r'@[^(]*', Text),
             (r'\^[^(]*', Text),
-            (r'(=)( +)([a-z]+)(\.)([a-z]+)', bygroups(Text, Text, Comment, Comment, Name.Function)),
-            (r'(!)([a-z]+)(\.)([a-z]+)', bygroups(Text, Comment, Comment, Text)),
+            (r'(=)( +)([a-z]+)(\.)([a-z]+)',
+             bygroups(Text, Text, Comment, Comment, Name.Function)),
+            (r'(!)([a-z]+)(\.)([a-z]+)', bygroups(Text, Comment, Comment,
+                                                  Text)),
             (r'(\n|\s)+', Text),
             (r'[=<>{}:\[\]()*.,!]|x\b', Text),
             (r'def', Text),
