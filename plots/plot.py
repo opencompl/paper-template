7611180a571e54d21095ffdbe65d211e13a3ee89
Tobias Grosser <tobias@grosser.es>
2022-11-13 01:55:43 -0800

    plotting: add argument parsing to only print one plot
    This will make it easier to selectively update plots in a paper.

--- plots/plot.py
+++ plots/plot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import os
 
 import matplotlib
@@ -17,13 +18,7 @@ def setGlobalDefaults():
     matplotlib.rcParams['ps.fonttype'] = 42
 
 
-setGlobalDefaults()
-
 matplotlib.rcParams['figure.figsize'] = 5, 2
-
-labels = ['G1', 'G2', 'G3', 'G4', 'G5']
-men_means = [1.5, 1.2, 1.3, 1.1, 1.0]
-women_means = [1.8, 1.5, 1.1, 1.3, 0.9]
 
 # Color palette
 light_gray = "#cacaca"
@@ -35,68 +30,95 @@ dark_green = "#33a02c"
 light_red = "#fb9a99"
 dark_red = "#e31a1c"
 
-x = np.arange(len(labels))  # the label locations
-width = 0.35  # the width of the bars
+# Plot an example speedup plot
+def plot_speedup():
+    labels = ['G1', 'G2', 'G3', 'G4', 'G5']
+    men_means = [1.5, 1.2, 1.3, 1.1, 1.0]
+    women_means = [1.8, 1.5, 1.1, 1.3, 0.9]
 
-fig, ax = plt.subplots()
-rects1 = ax.bar(x - width / 2, men_means, width, label='Men', color=light_blue)
-rects2 = ax.bar(x + width / 2,
-                women_means,
-                width,
-                label='Women',
-                color=dark_blue)
+    x = np.arange(len(labels))  # the label locations
+    width = 0.35  # the width of the bars
 
-# Y-Axis Label
-#
-# Use a horizontal label for improved readability.
-ax.set_ylabel('Speedup',
-              rotation='horizontal',
-              position=(1, 1.05),
-              horizontalalignment='left',
-              verticalalignment='bottom')
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x - width / 2,
+                    men_means,
+                    width,
+                    label='Men',
+                    color=light_blue)
+    rects2 = ax.bar(x + width / 2,
+                    women_means,
+                    width,
+                    label='Women',
+                    color=dark_blue)
 
-# Add some text for labels, title and custom x-axis tick labels, etc.
-ax.set_xticks(x)
-ax.set_xticklabels(labels)
+    # Y-Axis Label
+    #
+    # Use a horizontal label for improved readability.
+    ax.set_ylabel('Speedup',
+                  rotation='horizontal',
+                  position=(1, 1.05),
+                  horizontalalignment='left',
+                  verticalalignment='bottom')
 
-ax.legend(ncol=100,
-          frameon=False,
-          loc='lower right',
-          bbox_to_anchor=(0, 1, 1, 0))
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
 
-# Hide the right and top spines
-#
-# This reduces the number of lines in the plot. Lines typically catch
-# a readers attention and distract the reader from the actual content.
-# By removing unnecessary spines, we help the reader to focus on
-# the figures in the graph.
-ax.spines['right'].set_visible(False)
-ax.spines['top'].set_visible(False)
+    ax.legend(ncol=100,
+              frameon=False,
+              loc='lower right',
+              bbox_to_anchor=(0, 1, 1, 0))
+
+    # Hide the right and top spines
+    #
+    # This reduces the number of lines in the plot. Lines typically catch
+    # a readers attention and distract the reader from the actual content.
+    # By removing unnecessary spines, we help the reader to focus on
+    # the figures in the graph.
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+
+    def autolabel(rects):
+        """Attach a text label above each bar in *rects*, displaying its height."""
+        for rect in rects:
+            height = rect.get_height()
+            ax.annotate(
+                '{}'.format(height),
+                xy=(rect.get_x() + rect.get_width() / 2, height),
+                xytext=(0, 1),  # 1 points vertical offset
+                textcoords="offset points",
+                fontsize="smaller",
+                ha='center',
+                va='bottom')
+
+    autolabel(rects1)
+    autolabel(rects2)
+
+    fig.tight_layout()
 
 
-def autolabel(rects):
-    """Attach a text label above each bar in *rects*, displaying its height."""
-    for rect in rects:
-        height = rect.get_height()
-        ax.annotate(
-            '{}'.format(height),
-            xy=(rect.get_x() + rect.get_width() / 2, height),
-            xytext=(0, 1),  # 1 points vertical offset
-            textcoords="offset points",
-            fontsize="smaller",
-            ha='center',
-            va='bottom')
+    # Do not emit a creation date. This will make the content of
+    # the pdfs we generate more deterministic.
+    metadata = {'CreationDate': None}
+
+    fig.savefig("speedup.pdf", metadata=metadata)
 
 
-autolabel(rects1)
-autolabel(rects2)
+def main():
+    parser = argparse.ArgumentParser(
+        prog='plot',
+        description='Plot the figures for this paper',
+    )
+    parser.add_argument('names', nargs='+', choices=['all', 'speedup'])
+    args = parser.parse_args()
 
-fig.tight_layout()
+    setGlobalDefaults()
 
-filename = os.path.basename(__file__).replace(".py", ".pdf")
+    plotAll = 'all' in args.names
 
-# Do not emit a creation date. This will make the content of
-# the pdfs we generate more deterministic.
-metadata = {'CreationDate': None}
+    if 'speedup' in args.names or plotAll:
+        plot_speedup()
 
-fig.savefig(filename, metadata=metadata)
+
+if __name__ == "__main__":
+    main()
